1d671cf4ac2cdd1f95e7ec21f0cea0c4a496ece1
Philippe Trudel <p.trudel1996@gmail.com>
2017-07-26 09:26:45 -0400

    tutorial : Working with Databases - Creating an Action

---
+++ frontend/controllers/ContryController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use yii\data\Pagination;
+use app\models\Contry;
---
+++ frontend/views/site/about.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="col-md-3 col-md-offset-3">
+       <h1>simpletest</h1>
+    </div>
+
+</div>
