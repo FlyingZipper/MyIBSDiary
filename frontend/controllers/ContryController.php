15465544706f1145a772089346fede4f1c76e105
Philippe Trudel <p.trudel1996@gmail.com>
2017-07-26 09:36:11 -0400

    tutorial : Working with Databases - Creating an Action

--- frontend/controllers/ContryController.php
+++ frontend/controllers/ContryController.php
@@ -5,3 +5,15 @@ namespace app\controllers;
 use yii\web\Controller;
 use yii\data\Pagination;
 use app\models\Contry;
+
+class ContryController extends Controller{
+
+    public function actionIndex(){
+        $query = Contry::find();
+
+        $pagination = new Pagination([
+
+        ]);
+    }
+
+}
